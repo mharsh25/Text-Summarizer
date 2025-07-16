2e6d1606e5826813ff1896e0cba2cfcdc521fd43
mharsh25 <mishraharsh8654@gmail.com>
2025-07-16 15:38:31 +0530

    Data Ingestion modular implementation

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 
-logger.info("Welcome to custom log")
+STAGE_NAME="Data Ingestion Stage"
+try:
+    logger.info(f">>>>Stage {STAGE_NAME} started<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>Stage {STAGE_NAME} completed<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
